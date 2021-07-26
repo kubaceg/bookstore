8489bba3fe2b815ab6f1c77f32a3b029136c0c33
Jakub Cegiełka <kuba.ceg@gmail.com>
2021-07-26 21:30:19 +0200

    Book reservation/release feature (#2)
    * Add common tests
    * Reserve books

--- internal/books/repository/book.go
+++ internal/books/repository/book.go
@@ -5,14 +5,24 @@ import (
 	"errors"
 )
 
+type BookState int32
+
+const (
+	Available    BookState = 0
+	Reserved               = 1
+	NotAvailable           = 2
+)
+
 var BookNotFound = errors.New("book not found")
 var BookAlreadyExists = errors.New("book already exists")
+var BookAlreadyReserved = errors.New("book already reserved")
 
 type BookEntity struct {
-	Id     string `json:"id,omitempty"`
-	Title  string `json:"title,omitempty"`
-	Author string `json:"author,omitempty"`
-	Isbn   string `json:"isbn,omitempty"`
+	Id     string    `json:"id,omitempty"`
+	Title  string    `json:"title,omitempty"`
+	Author string    `json:"author,omitempty"`
+	Isbn   string    `json:"isbn,omitempty"`
+	State  BookState `json:"state,omitempty"`
 }
 
 type BookListParams struct {
@@ -24,4 +34,6 @@ type BookRepository interface {
 	AddBook(ctx context.Context, book BookEntity) (id *string, err error)
 	GetBook(ctx context.Context, id string) (book *BookEntity, err error)
 	GetBookList(ctx context.Context, params BookListParams) (list []BookEntity, err error)
+	ReserveBook(ctx context.Context, id string) (err error)
+	ReleaseBook(ctx context.Context, id string) (err error)
 }
